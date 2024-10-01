a7eeca41eac14b9d92e451309dd8ccef89497d00
Tim Sameke <timothy.sameke@news.co.uk>
2024-10-01 14:35:06 +0100

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ChangeEvent, useState } from "react";
 
 function App() {
+  const [results, setResults] = useState<any>([]);
+  const [search, setSearch] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  function handleSearchInput (event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  };
+  
+  async function handleBtnClick(){
+    try {
+      const url = 'https://restcountries.com/v3.1/name';
+      const res = await fetch(`${url}/${search}`);
+      if (res.status !== 200) throw new Error("Fetch falied");
+      const data = await res.json();
+      setResults(data);
+    } 
+    catch (error: any) {
+      if (error?.message) setErrorMsg(error.message);
+    }
+  };
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Country search</h2>
       </header>
-    </div>
+      <div className='Search'>
+        <input
+            type="text"
+            placeholder="Search..."
+            onChange={(event) => {
+              handleSearchInput(event);
+            }}
+            className='searchInput'
+          />
+        <button
+          onClick={() => {
+            handleBtnClick();
+          }}
+          className='button'
+        >
+          Search
+        </button>
+      </div>
+      { !errorMsg && <div id="search-results-container">
+        {results.map((x: any) => {
+          return (
+            <div id="search-result">
+              {x.flag} {x.name.official}
+              <br />
+              Capital: {x.capital}
+            </div>
+          );
+        })}
+      </div>
+      }
+      { errorMsg && 
+        <div>
+          <span className='errorMsg'> {errorMsg} </span>
+        </div>
+      }
+    </>
   );
 }
 
