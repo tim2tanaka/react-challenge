525289c39c5143d0dcca2ad2d31974b1a95a4f62
Tim Sameke <timothy.sameke@news.co.uk>
2024-10-01 15:03:13 +0100

    update npm packages, add snapshot test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe ('App', ()=> {
+  test('render App', () => {
+    const { container } = render(<App />)
+    expect(container).toMatchSnapshot()
+
+  });
   test('renders header', () => {
     render(<App />);
     const headerElement = screen.getByText(/Country search/i);
